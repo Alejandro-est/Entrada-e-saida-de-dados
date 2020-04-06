91d5a6a41466d65535d337a052b174b465d185f2
Alejandro R. E. Cristiano <alejandro.estevez.cristiano@gmail.com>
2020-04-06 19:35:17 -0300

    modificacoes

---
+++ desafio5.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+   float s_l,h_t,v_h,s_b;
+
+   printf("Qual a quantidade de horas trabalhadas no mes?\n");
+   scanf("%f",&h_t);
+   
+   system("pause");
+    return 0;
+
+
+}
